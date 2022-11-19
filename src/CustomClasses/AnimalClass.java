115b31882d037e17d0322383f7a38d1cbb4631ae
zungun1989 <zbezgin1989@gmail.com>
2022-11-18 19:58:44 -0700

    all

---
+++ src/CustomClasses/AnimalClass.java
@@ -0,0 +1,37 @@
+package CustomClasses;
+
+public class AnimalClass {
+    public String AnimalName;
+    public String AnimalType;
+
+    public String AnimalColor;
+
+    ///This is a constructor. This will be called when you create Class
+    ///@param1 is a String variable and we will use it as AnimalName
+    ///@param2 is a String variable and we will use it as AnimalType
+
+    public AnimalClass(String param1, String param2){
+        AnimalName = param1;
+        AnimalType = param2;
+    }
+
+    //How to define a Constructor
+    //public ClassName(ParameterType ParameterName, ....)
+
+    ///This is a constructor. This will be called when you create Class
+    ///@param1 is a String variable and we will use it as AnimalName
+    ///@param2 is a String variable and we will use it as AnimalType
+    ///@param3 is a String variable and we will use it as AnimalColor
+
+    public AnimalClass(String param1, String param2,String param3){
+        AnimalName = param1;
+        AnimalType = param2;
+        AnimalColor = param3;
+    }
+
+
+
+    public void AMethod (String param3){
+        System.out.println(param3);
+    }
+}
---
+++ src/Day26_ClassesAndConstructors/Topic3_StudentExample.java
@@ -0,0 +1,31 @@
+package Day26_ClassesAndConstructors;
+
+import CustomClasses.AnimalClass;
+import CustomClasses.StudentClass;
+
+public class Topic3_StudentExample {
+    public static void main(String[] args) {
+        StudentClass MyStudent = new StudentClass();
+        MyStudent.Name = "Zeliha";
+        MyStudent.StudentNumber = "928";
+
+        AnimalClass MyAnimal = new AnimalClass("Lion", "Cat");
+        MyAnimal.AMethod("Something");
+        System.out.println(MyAnimal.AnimalName);
+        MyAnimal.AnimalName = "Leopar";
+        System.out.println(MyAnimal.AnimalName+ " "+ MyAnimal.AnimalType);
+
+        AnimalClass MyTiger = new AnimalClass("Tiger", "Cat");
+        System.out.println(MyTiger.AnimalName);
+
+        /*
+        I don't need this code as I can define the name and type on Created class
+        MyAnimal.AnimalName = "Lion";
+        MyAnimal.AnimalType = "Cat";
+
+         */
+
+        AnimalClass MyPenguin = new AnimalClass("Penguin", "Bird", "White-Black");
+        System.out.println(MyPenguin.AnimalName+" "+ MyPenguin.AnimalType+ " "+MyPenguin.AnimalColor);
+    }
+}
