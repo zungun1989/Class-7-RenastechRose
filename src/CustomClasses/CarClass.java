162c4a86b9c68dbe279067dc5ca16d2d5234b103
zungun1989 <zbezgin1989@gmail.com>
2022-11-18 18:24:11 -0700

    all

---
+++ src/CustomClasses/CarClass.java
@@ -0,0 +1,11 @@
+package CustomClasses;
+
+public class CarClass {
+    //fields
+    String Make;
+    String Model;
+    int ModelYear;
+    String Color;
+
+
+}
---
+++ src/Day26_ClassesAndConstructors/Topic1_PublicClasses.java
@@ -0,0 +1,7 @@
+package Day26_ClassesAndConstructors;
+
+public class Topic1_PublicClasses {
+    public static void main(String[] args) {
+
+    }
+}
