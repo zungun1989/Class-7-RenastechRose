28c7f450cc2aea2cb8b60fe31f83dde5f3800a24
zungun1989 <zbezgin1989@gmail.com>
2022-12-01 18:13:06 -0700

    all

--- src/Day31_CollectionsFramework/Topic3_ArrayListRandomAccess.java
+++ src/Day31_CollectionsFramework/Topic3_ArrayListRandomAccess.java
@@ -5,6 +5,34 @@ import java.util.ArrayList;
 public class Topic3_ArrayListRandomAccess {
     public static void main(String[] args) {
         ArrayList<Integer> MyArrayList = new ArrayList<>();
+        MyArrayList.add(8476);
+        MyArrayList.add(2);
+        MyArrayList.add(8476);
+        MyArrayList.add(4);
+        MyArrayList.add(1);
+        MyArrayList.add(1);
+        MyArrayList.add(999);
+        MyArrayList.add(1794);
+        MyArrayList.add(null);
+
+        System.out.println(MyArrayList);
+
+        /*
+        The ArrayList class implements the List interface.
+        It uses a dynamic array to store the duplicate element of different data types.
+        (Increase the size of array based on demand)
+        The ArrayList class maintains the insertion order.
+        The elements stored in the ArrayList class can be randomly accessed.
+
+
+        The purpose of RandomAccess Interface is to retrieve any random element in the Collection at the same speed.
+        Example: I have a collection of 1 million objects.
+        Implementing RandomAccess interface makes your time to retrieve the 10th element and 17869th element the same.
+        This makes ArrayList more powerful.
+         */
+
+        System.out.println(MyArrayList.get(0));
+        MyArrayList.get(8);
 
     }
 }
