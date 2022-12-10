0bbe34534bb4a5b530deb1f25e91f3e95cd07b2a
zungun1989 <zbezgin1989@gmail.com>
2022-12-09 19:42:15 -0700

    all

--- src/Day33_StringBuilderAndFileOperations/Topic7_Examples.java
+++ src/Day33_StringBuilderAndFileOperations/Topic7_Examples.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Topic7_Examples {
     public static void main(String[] args) {
@@ -11,15 +13,80 @@ public class Topic7_Examples {
             FileReader FR = new FileReader("C:/Users/Lenovo/Desktop/Day33/top250.txt");
             BufferedReader MyReader = new BufferedReader(FR);
 
+            ArrayList<Movie> Top250 = new ArrayList<>();
+
             String line = MyReader.readLine();
+
             while (line != null){
-                System.out.println(line);
-                 line = MyReader.readLine();
+                //9.2  The Shawshank Redemption (1994)
+                StringBuilder SB = new StringBuilder(line);
+                // String s = new String("something")
+
+                String Rating = SB.substring(0, 3);
+                System.out.println("Rating : "+Rating);
+
+                SB.delete(0,5);
+
+                //The Shawshank Redemption (1994)
+
+                int Position1 = SB.indexOf("(");
+                String Movie = SB.substring(0, Position1 - 1);
+
+
+                System.out.println("Movie : "+Movie);
+                SB.delete(0, Position1);
+
+                //(1994)
+
+                String Year = SB.substring(1, 5);
+                System.out.println("Year : "+Year);
+
+                //Another way to get year
+                //int Position2 = SB.indexOf(")");
+                //String Year = SB.substring(1, Position2);
+
+                Movie aMovie = new Movie();
+                aMovie.MovieName = Movie;
+                aMovie.Rating = Rating;
+                aMovie.Year = Year;
+
+                Top250.add(aMovie);
+
+//                System.out.println(SB);
+                line = MyReader.readLine();
             }
-        }catch (FileNotFoundException e){
+
+            System.out.println(Top250.size());
+            System.out.println("*********************");
+            PrintOldMovies(Top250);
+
+
+        } catch (FileNotFoundException e){
             System.out.println("File Not Found!");
-        }catch (IOException e){
-            System.out.println("Error while reading file!");
+        } catch (IOException e) {
+            System.out.println("Error while reading file");
         }
     }
+
+    ///please create a method that takes an arraylist of movie as parameter and prints movies that older than 1999
+    ///method name = PrintOldMovies
+    /// return type  = void
+    // publicorprivate staticornot returntype methodname(parameter1type parameter1name, ........)
+    public static void PrintOldMovies(ArrayList<Movie> param1){
+        // prints movies that older than 1999
+        for (Movie MyMovieInTheList:param1) {
+            if(Integer.parseInt(MyMovieInTheList.Year) < 1999 ){
+                System.out.println(MyMovieInTheList.MovieName);
+            }
+
+        }
+    }
+
+
+}
+
+class Movie {
+    String Year;
+    String MovieName;
+    String Rating;
 }
